112235e7179f5cd982ad4aedb8d04de8b8122cfa
dropspindl <edelmaned@gmail.com>
2018-11-12 15:29:23 -0800

    Finish first routes project

--- first_routes/app/models/user.rb
+++ first_routes/app/models/user.rb
@@ -11,5 +11,9 @@
 
 class User < ApplicationRecord
   validates :email, presence: true
+  validates :email, uniqueness: true 
+  validates :name, presence: true
+  validates :name, uniqueness: true 
+  
   
 end
